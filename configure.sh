4dece0259d618f9eb53198299f0c6921c9fffe18
nicolaslucius3432 <114325416+nicolaslucius3432@users.noreply.github.com>
2022-09-25 04:39:36 +0000

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# V2Ray new configuration
+# Run V2ray
+# Write V2Ray configuration
+cat << EOF > /etc/v2ray/config.json
+{
+    "inbounds": [{
+        "port": 8081,
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "2992b642-ff6c-4121-bg42-d4310ak532hf",
+                "alterId": 0
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "/vmess"
+            }
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
+#Run V2ray
+
+/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
